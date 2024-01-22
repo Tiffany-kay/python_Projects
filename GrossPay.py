f08f9ac0596a230817cc8c49d933732497c3732d
tiffany <gathonitiffany16@gmail.com>
2024-01-22 16:35:46 +0300

    first commit

---
+++ GrossPay.py
@@ -0,0 +1,7 @@
+# code to calculate gross_pay
+hours = float(input("Enter hours worked: "))  # user input
+rate = float(input("Enter rate per hour: "))  # user input
+# formula to calculate gross_pay
+gross_pay = hours * rate
+# output
+print("Gross_pay = ", gross_pay)
